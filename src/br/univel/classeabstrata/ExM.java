beddff157690691ae80fbc7d571c32826ca1e317
Fabio <kucinskifk@gmail.com>
2016-03-20 13:17:37 -0300

    Exercico M

---
+++ src/br/univel/classeabstrata/ExM.java
@@ -0,0 +1,45 @@
+package br.univel.classeabstrata;
+
+
+
+// public class ExM implements Desenho {
+ class ExM extends DesenhoAbstrato {
+
+	public static int SIZE = 8;
+	
+
+	
+	@Override
+	protected void desenhar() {
+		
+	
+
+		
+		for (int linha = 1; linha <= SIZE; linha++) {
+			for (int col = 1; col <= SIZE; col++) {
+				if (col > linha) {
+					System.out.print(" ");
+				} else {
+					System.out.print(col + " ");
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+
+
+
+	@Override
+	protected String getNome() {
+		// TODO Auto-generated method stub
+		return "M";
+	}
+	
+	
+//	@Override
+//	protected String getNome() {
+//		return "m";
+//	}
+
+
+}
